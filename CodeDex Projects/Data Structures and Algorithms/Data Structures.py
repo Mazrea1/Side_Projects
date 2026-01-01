f335085a2482fdc82a844bdf47048bc588246ee0
Mazreal23 <michael.lalaj@outlook.com>
2025-12-31 19:07:54 -0600

    Completed Version 1

---
+++ CodeDex Projects/Data Structures and Algorithms/Data Structures.py
@@ -0,0 +1,11 @@
+friends = ['apple', 'karma', 'raymond']
+song = {
+    'name' : 'Too Easy',
+    'artist' : 'Connor Price',
+    'year' : 2025
+}
+places = {'New York', 'Tokyo', 'Paris'}
+
+print(friends[2])
+print(song['name'])
+print('Paris' in places)
